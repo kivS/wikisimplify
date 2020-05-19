461e3f33f1a2efc9556eb1c6ca5eff24e5b49fa1
Vik <v1k@protonmail.com>
2020-05-19 16:48:29 +0100

    Add toggle for simplify (#5)
    * added toggle function for simplification;
    
    * fixed wikipedia simplify test
    
    * made comments clearer and prettified the testing output

--- chrome/background.js
+++ chrome/background.js
@@ -4,7 +4,7 @@
 **/
 
 
-// Register pageAction activate only when user visits wikipedia.org site
+// Register pageAction: activate only when user visits wikipedia.org site
 chrome.runtime.onInstalled.addListener(function(details) {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([
@@ -22,12 +22,17 @@ chrome.runtime.onInstalled.addListener(function(details) {
 
 
 /**
-# Helper function to add the `simple` keyword into the 
-# wikipedia url.
+# Helper function to add the `simple` keyword in the wikipedia url.
+# If the article already is the simplified version(have `simple` in url) then, triggering the extension 
+# again will cause it to reverse the change and return the previous link
 # 
 # Example:
 #  input(String)  -> https://en.wikipedia.org/wiki/Kinetic_energy
 #  output(String) -> https://simple.wikipedia.org/wiki/Kinetic_energy
+#
+# Reverse:
+#  input(String) -> https://simple.wikipedia.org/wiki/Kinetic_energy
+#  output(String)  -> https://en.wikipedia.org/wiki/Kinetic_energy
 **/
 function simplify_wikipedia(url){
 
@@ -35,12 +40,28 @@ function simplify_wikipedia(url){
 
 	let parsed_url_object = new URL(url)
 
-	// split the url hostname into an array and create a new array without the words that are not in the `WORDS_ALLOWED_IN_HOSTNAME` list
+	// split the url hostname into an array 
 	const splitted_hostname = parsed_url_object.hostname.split('.')
-	let filtered_splitted_hostname = splitted_hostname.filter(splitted_words => WORDS_ALLOWED_IN_HOSTNAME.some(allowed_word => splitted_words.includes(allowed_word)))
+
+	let filtered_splitted_hostname = []
+
+	// if the first item in the hostname is the `simple` keyword, then we want to toggle the simplification and replace it with `en` for English
+	if(splitted_hostname[0] == 'simple'){
+		console.log('Article is already in the simple version. let\'s toggle it back to normal')
+		splitted_hostname[0] = 'en'
+		filtered_splitted_hostname = splitted_hostname
+
+
+	}else{
+
+		// Create a new array without the words that are not in the `WORDS_ALLOWED_IN_HOSTNAME` list
+		filtered_splitted_hostname = splitted_hostname.filter(splitted_words => WORDS_ALLOWED_IN_HOSTNAME.some(allowed_word => splitted_words.includes(allowed_word)))
 	
-	// add `simple` word into the splitted hostname as the first item
-	filtered_splitted_hostname = ["simple", ...filtered_splitted_hostname]
+		// add `simple` word into the splitted hostname as the first item
+		filtered_splitted_hostname = ["simple", ...filtered_splitted_hostname]
+
+	}
+
 
 	// replace old hostname from url object with the filtered one
 	const filtered_hostname_string = filtered_splitted_hostname.join('.')
@@ -77,14 +98,24 @@ function test_simplify_wikipedia(){
 		},
 		{ 
 			'input': 'https://simple.wikipedia.org/wiki/Albert_Einstein', 
-			'expected_output': 'https://simple.wikipedia.org/wiki/Albert_Einstein'
-		}
+			'expected_output': 'https://en.wikipedia.org/wiki/Albert_Einstein'
+		},
+		{ 
+			'expected_output': 'https://en.wikipedia.org/wiki/Kinetic_energy', 
+			'input': 'https://simple.wikipedia.org/wiki/Kinetic_energy'
+		},
+		
 	]
 
 	usecase_list.map(usecase => {
-		console.log(`testing usecase for url: ${usecase.input}`)
+		console.dir(usecase)
 		output = simplify_wikipedia(usecase.input)
 		// assertion and returns a context object in case of assertion error
-		console.assert(usecase.expected_output == output, {test_usecase:usecase, output: output})
+		if(usecase.expected_output != output){
+			throw new Error(`❌ Output:${output} != expected:${usecase.expected_output}`)
+		}else{
+			console.log('✅')
+		}
+
 	})
 }
